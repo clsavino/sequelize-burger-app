7ecde23cab73a6ff3f29801dec4fbc122cdca4ba
Christi Savino <c.savino@ymail.com>
2016-11-05 19:17:50 -0400

    upload files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,62 @@
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();
+var model = require('../models');
+
+//model.sync({force:true});
+
+//root route redirect to /burgers
+router.get('/', function (req, res) {
+  res.redirect('/burgers');
+});
+
+// display all burgers route
+router.get('/burgers', function (req, res) {
+  model.burger.findAll()
+  .then(function(data) {
+    res.render("index", {burger : data});
+  });
+});
+
+
+// Insert route inserts new burger in database - '/burgers/new'
+router.post('/burgers/insertOne', function (req, res) {
+  var newBurger = req.body.name;
+  model.burger.create({
+    burger_name: newBurger,
+    devoured: false
+  })
+  .then(function () {
+    res.redirect("/burgers");
+  });
+});
+
+// Update route updates the devoured field to true
+router.put('/burgers/updateOne/:id', function (req, res) {
+  var burgerId = req.params.id;
+  model.burger.update({
+    devoured: true
+  },
+  {
+    where: {
+      id: burgerId
+    }
+  }).then(function () {
+    res.redirect("/burgers");
+  });
+});
+
+router.delete('/burgers/delete/:id', function (req, res) {
+  var burgerId = req.params.id;
+  model.burger.destroy({
+    where: {
+      id: burgerId
+    }
+    }) .then(function() {
+      res.redirect('/burgers');
+  });
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### Schema
+CREATE DATABASE seq_burgers_db;
+USE seq_burgers_db;
+
+CREATE TABLE burgers
+(
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
+
+
+
